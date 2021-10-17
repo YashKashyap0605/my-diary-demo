17d19c86c2446cc5697705e02babd59ae11c54ca
YashKashyap0605 <yashkashyap0605@gmail.com>
2021-10-17 13:19:06 +0530

    First Commit

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const ejs = require("ejs");
 const mongoose = require('mongoose');
 
 const homeStartingContent = "This Area contains all the posts made by you :)";
-const aboutContent = "This is just a mini project named as MY PERSONAL DIARY . It allows you to COMPOSE your content with your desired TITLE and CONTENT. Composing post is a very easy task you just have to type in the desired text and you will easily be able to post it on the home page just by clicking on the button. It keeps al your data private and no one else has access to it. For any queries and suggestions you can move to the CONTACT US section and find my email id and send in your suggestions along with your username and stuff. The HOME sections simply contains all the contents composed by you along with the titles in it. Hope you enjoy using my simple project. Currently I am not allowing users to add images because I have only a limited 512mb provided by the MONGODB server for free usage and as I am just a college student I cannot pay for the server costs so Sorry for that. Updates will be made just keep supporting . THANK YOU.";
-const contactContent = "My Personal Email: yashkashyap0605@gmail.com. Feel free to drop by your suggestions for further updates :)";
+const aboutContent = "This is just a mini project named as MY DIARY .";
+const contactContent = "";
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-kashyap:kingman123...@cluster0.jc0d8.mongodb.net/myDiary?retryWrites=true&w=majority", {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
